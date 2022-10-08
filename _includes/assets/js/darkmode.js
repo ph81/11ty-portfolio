658e968887d45118aa9a8d45f8890ccfd2987873
Edith M <paperhearts81@gmail.com>
2022-10-08 12:04:56 -0500

    feat: dark mode toggle added

---
+++ _includes/assets/js/darkmode.js
@@ -0,0 +1,36 @@
+const showTheme = () => {
+  const toggle = document.getElementById("theme-toggle");
+  const iconToggle = document.getElementById("icon-toggle");
+
+  let storedTheme = localStorage.getItem('theme') || (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light");
+  console.log(storedTheme);
+  if (storedTheme)
+      document.documentElement.setAttribute('data-theme', storedTheme)
+
+
+  toggle.onclick = () => {
+      let currentTheme = document.documentElement.getAttribute("data-theme");
+      let targetTheme = "light";
+      
+      if (currentTheme === "light") {
+          targetTheme = "dark";
+      } 
+      document.documentElement.setAttribute('data-theme', targetTheme)
+      localStorage.setItem('theme', targetTheme);
+  }
+}
+
+const showContent = () => {
+  document.body.style.visibility = 'visible';
+  document.body.style.opacity = 1;
+}
+
+
+  // listen to the DOM content loaded or not?
+window.addEventListener('DOMContentLoaded', (event) => {
+  showTheme();
+  showContent();
+});
+
+
+
